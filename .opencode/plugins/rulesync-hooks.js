1fecd93edcff0578fc7abc5ce3784aaa09d9e75b
Masatora Atarashi <atarashi.masatora@gmail.com>
2026-09-24 13:07:36 +0900

    feat: Everyday Japan inbound tour LP from public-lp

---
+++ .opencode/plugins/rulesync-hooks.js
@@ -0,0 +1,12 @@
+export const RulesyncHooksPlugin = async ({ $ }) => {
+  return {
+    "tool.execute.before": async (input) => {
+      {
+        const __re = new RegExp("Bash");
+        if (__re.test(input.tool)) {
+          await $`.rulesync/hooks/security-guard.sh`;
+        }
+      }
+    },
+  };
+};
